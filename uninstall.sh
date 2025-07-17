5a7e8e4f270f24a7d99cd43e9b9820e7bbdb4096
Seyud <2159455958@qq.com>
2025-07-18 05:26:46 +0800

    refactor: 简化卸载脚本并添加配置目录清理

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
-# 删除配置文件
-rm -f /data/gpu_freq_table.conf
-# 删除日志目录（包含日志等级文件）
+# 删除日志目录
 rm -rf /data/adb/gpu_governor/log
-# 删除游戏目录（包含游戏列表和游戏模式文件）
+# 删除游戏目录
 rm -rf /data/adb/gpu_governor/game
+# 删除配置目录
+rm -rf /data/adb/gpu_governor/config
