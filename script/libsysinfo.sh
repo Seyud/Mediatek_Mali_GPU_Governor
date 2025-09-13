3cc73ea1a8c121404a81634f6b08bbca81af630f
Seyud <2159455958@qq.com>
2025-09-14 02:07:26 +0800

    refactor(script): 移除非MTK平台相关代码并更新注释

--- script/libsysinfo.sh
+++ script/libsysinfo.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 
 ###############################
-# Platform info functions
+# MTK Platform info functions
 ###############################
 
 # $1:"4.14" return:string_in_version
@@ -57,50 +57,6 @@ is_mtk() {
     [ $? -eq 0 ] && echo "true" || echo "false"
 }
 
-_get_sm6150_type() {
-    case "$(get_socid)" in
-        365 | 366) echo "sdm730" ;;
-        355 | 369) echo "sdm675" ;;
-    esac
-}
-
-_get_sdm76x_type() {
-    if [ "$(get_maxfreq 7)" -gt 2300000 ]; then
-        echo "sdm765"
-    else
-        echo "sdm750"
-    fi
-}
-
-_get_msm8916_type() {
-    case "$(get_socid)" in
-        "206" | "247" | "248" | "249" | "250") echo "msm8916" ;;
-        "233" | "240" | "242") echo "sdm610" ;;
-        "239" | "241" | "263" | "268" | "269" | "270" | "271") echo "sdm616" ;;
-        *) echo "msm8916" ;;
-    esac
-}
-
-_get_lahaina_type() {
-    if [ "$(get_maxfreq 7)" -gt 2600000 ]; then
-        echo "sdm888"
-    else
-        if [ "$(get_maxfreq 4)" -gt 2300000 ]; then
-            echo "sdm778"
-        else
-            echo "sdm780"
-        fi
-    fi
-}
-
-_get_taro_type() {
-    if [ "$(get_maxfreq 4)" -gt 2700000 ]; then
-        echo "sdm8g1+"
-    else
-        echo "sdm8g1"
-    fi
-}
-
 # 区分mt6891和mt6893
 _get_mt689x_type() {
     # 如果是mt6893但CPU7最大频率小于2700000，则实际是mt6891
@@ -114,43 +70,6 @@ _get_mt689x_type() {
 # $1:board_name
 get_config_name() {
     case "$1" in
-        "taro") echo "$(_get_taro_type)" ;;
-        "lahaina") echo "$(_get_lahaina_type)" ;;
-        "shima") echo "$(_get_lahaina_type)" ;;
-        "yupik") echo "$(_get_lahaina_type)" ;;
-        "kona") echo "sdm865" ;;    # 865, 870
-        "msmnile") echo "sdm855" ;; # 855, 860
-        "sdm845") echo "sdm845" ;;
-        "lito") echo "$(_get_sdm76x_type)" ;;
-        "sm6150") echo "$(_get_sm6150_type)" ;;
-        "sdm710") echo "sdm710" ;;
-        "msm8916") echo "$(_get_msm8916_type)" ;;
-        "msm8939") echo "sdm616" ;;
-        "msm8953") echo "sdm625" ;;    # 625
-        "msm8953pro") echo "sdm625" ;; # 626
-        "sdm660") echo "sdm660" ;;
-        "sdm636") echo "sdm660" ;;
-        "trinket") echo "sdm665" ;; # sdm665
-        "bengal") echo "sdm665" ;;  # sdm662
-        "msm8976") echo "sdm652" ;;
-        "msm8956") echo "sdm650" ;;
-        "msm8998") echo "sdm835" ;;
-        "msm8996") echo "sdm820" ;;
-        "msm8996pro") echo "sdm820" ;;
-        "s5e9925") echo "e2200" ;;
-        "exynos2100") echo "e2100" ;;
-        "exynos1080") echo "e1080" ;;
-        "exynos990") echo "e990" ;;
-        "universal9925") echo "e2200" ;;
-        "universal2100") echo "e2100" ;;
-        "universal1080") echo "e1080" ;;
-        "universal990") echo "e990" ;;
-        "universal9825") echo "e9820" ;;
-        "universal9820") echo "e9820" ;;
-        "universal9810") echo "e9810" ;;
-        "universal8895") echo "e8895" ;;
-        "universal8890") echo "e8890" ;;
-        "universal7420") echo "e7420" ;;
         "mt6765") echo "mtp35" ;; # Helio P35(mt6765)/G35(mt6765g)/G37(mt6765h)
         "mt6768") echo "mtg80" ;; # Helio P65(mt6768)/G70(mt6769v)/G80(mt6769t)/G85(mt6769z)
         "mt6785") echo "mtg90t" ;;
@@ -168,7 +87,6 @@ get_config_name() {
         "mt6895") echo "mtd8100" ;;
         "mt6896") echo "mtd8200" ;;
         "mt6983") echo "mtd9000" ;;
-        "gs101") echo "gs101" ;;
         *) echo "unsupported" ;;
     esac
 }
