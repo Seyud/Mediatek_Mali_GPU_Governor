0664d23f786b3074626b1d8d0244887ed4d3e4f0
Seyud <2159455958@qq.com>
2025-09-17 18:46:35 +0800

    fix(script): 添加错误信息以改善安装失败时的用户反馈

--- customize.sh
+++ customize.sh
@@ -2,7 +2,7 @@
 
 on_install() {
     sh "$MODPATH"/script/setup.sh 2>&1
-    [ "$?" != "0" ] && abort
+    [ "$?" != "0" ] && abort "调速器安装失败！"
 
     rm "$MODPATH"/script/setup.sh
     rm "$MODPATH"/config/
